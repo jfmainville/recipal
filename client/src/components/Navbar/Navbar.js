926e2f01b58eabadbe85ee6d7d1d75de252d4f44
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-01-06 12:40:58 -0500

    Refactor Navbar.js component to the final structure
    Refactor the Navbar.js component to the final structure without any connection to the state for now.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,29 @@
 import React from "react";
 import classes from "./Navbar.module.scss";
 import { NavLink } from "react-router-dom";
+import InputField from "../UI/InputField/InputField";
+import Button from "../UI/Button/Button";
 
 const navbar = () => {
 	return (
 		<React.Fragment>
-			<div className={classes.Logo}>Recipal</div>
+			<div className={classes.Logo}>
+				<NavLink
+					activeClassName={classes.NavigationItemLinkActive}
+					className={classes.NavigationItemLink}
+					to={"/"}
+				>
+					Recipal
+				</NavLink>
+			</div>
+			<form className={classes.SearchBar}>
+				<InputField
+					placeHolder="carbonara, steak..."
+				/>
+				<Button
+					buttonText="Search"
+				/>
+			</form>
 			<div className={classes.Logout}>
 				<NavLink
 					activeClassName={classes.NavigationItemLinkActive}
