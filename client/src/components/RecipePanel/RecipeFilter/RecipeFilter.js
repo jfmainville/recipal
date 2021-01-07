3744f1c327c7d8831edd5de2f27c0654b875ae7d
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-01-07 16:40:44 -0500

    Add the RecipeFilter.js component to the project
    Add the RecipeFilter.js component to the project.

---
+++ client/src/components/RecipePanel/RecipeFilter/RecipeFilter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./RecipeFilter.module.scss";
+
+const RecipeFilter = props => {
+	return (
+		<div className={classes.RecipeFilter}>
+			Recipes Filter
+		</div>
+	);
+};
+
+export default RecipeFilter;
