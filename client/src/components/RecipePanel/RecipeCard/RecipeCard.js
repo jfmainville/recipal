018fa30998f6cf8e26ef15702e8017b1f1a9f40e
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-01-08 09:56:40 -0500

    Refactor the RecipeCard.js component structure
    Refactor the RecipeCard.js component structure to redirect to the RecipeDetailsPanel.js component.

--- client/src/components/RecipePanel/RecipeCard/RecipeCard.js
+++ client/src/components/RecipePanel/RecipeCard/RecipeCard.js
@@ -1,17 +1,27 @@
 import React from "react";
 import classes from "./RecipeCard.module.scss";
+import { NavLink } from "react-router-dom";
 
 const recipeCard = props => {
 	const recipe = props.recipe;
 
 	return (
-		<div className={classes.RecipeCard}>
-			<img alt={recipe.name} width="200px" height="200px" src={`data:image/jpeg;base64,${recipe.image}`}/>
-			<div className={classes.RecipeCardInformation}>
-				<p>{recipe.name}</p>
-				<button>X</button>
+		<React.Fragment>
+			<div className={classes.RecipeCard}>
+				<NavLink
+					activeClassName={classes.NavigationItemLinkActive}
+					className={classes.NavigationItemLink}
+					to={"/" + recipe.id}
+				>
+					<img alt={recipe.name} width="200px" height="200px" src={`data:image/jpeg;base64,${recipe.image}`}/>
+				</NavLink>
+				<div className={classes.RecipeCardInformation}>
+					<p>{recipe.name}</p>
+					<button>X</button>
+				</div>
 			</div>
-		</div>
+		</React.Fragment>
+
 	);
 };
 
