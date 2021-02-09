9d07089d39b7a1d987e4e66cd63247dfefb0ea2c
Jean-Frederic Mainville <jfmainville@outlook.com>
2021-02-08 21:12:28 -0500

    Add the NewRecipePanel.js component
    Add the NewRecipePanel.js component to the RecipePanel.js component.
    
    - Add the Modal.js component
    - Migrate all the functions under the NewRecipePanel.js component
    - Add the logic to support showing and hiding the Modal.js component

--- client/src/components/RecipePanel/RecipePanel.js
+++ client/src/components/RecipePanel/RecipePanel.js
@@ -3,10 +3,21 @@ import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../../store/actions/index";
 import classes from "./RecipePanel.module.scss";
 import RecipeCard from "./RecipeCard/RecipeCard";
+import { v4 as uuidv4 } from "uuid";
+import NewRecipePanel from "./NewRecipePanel/NewRecipePanel";
+import Modal from "../UI/Modal/Modal";
 
 const RecipePanel = () => {
 	const recipes = useSelector(state => state.recipe.recipes);
 	const dispatch = useDispatch();
+	const [showModal, setShowModal] = useState(false);
+	const [recipeNameInput, setRecipeNameInput] = useState("");
+	const [recipePreparationTimeInput, setRecipePreparationTimeInput] = useState("");
+	const [recipeCookTimeInput, setRecipeCookTimeInput] = useState("");
+
+	const handleShowModal = () => {
+		setShowModal(!showModal);
+	};
 
 	useEffect(() => {
 		dispatch(actions.fetchRecipes());
@@ -20,8 +31,53 @@ const RecipePanel = () => {
 		}
 	};
 
+	const handleRecipeNameInput = (data) => {
+		setRecipeNameInput(data);
+	};
+
+	const handleRecipePreparationTimeInput = (data) => {
+		setRecipePreparationTimeInput(data);
+	};
+
+	const handleRecipeCookTimeInput = (data) => {
+		setRecipeCookTimeInput(data);
+	};
+
+	const handleRecipeCreate = () => {
+		let data = {};
+		if (recipeNameInput !== "" && recipePreparationTimeInput !== "" && recipeCookTimeInput !== "") {
+			data.id = uuidv4();
+			data.name = recipeNameInput;
+			data.image = "/9j/4AAQSkZJRgABAQEAYABgAAD";
+			data.preparation_time = recipePreparationTimeInput;
+			data.cook_time = recipeCookTimeInput;
+			dispatch(actions.createRecipe(data));
+			setRecipeNameInput(null);
+			setRecipePreparationTimeInput(null);
+			setRecipeCookTimeInput(null);
+			handleShowModal();
+		}
+	};
+
 	return (
 		<React.Fragment>
+			{showModal ?
+				<Modal
+					showModal={showModal}
+					handleShowModal={handleShowModal}
+				>
+					<NewRecipePanel
+						handleRecipeNameInput={handleRecipeNameInput}
+						recipeNameInput={recipeNameInput}
+						handleRecipePreparationTimeInput={handleRecipePreparationTimeInput}
+						recipePreparationTimeInput={recipePreparationTimeInput}
+						handleRecipeCookTimeInput={handleRecipeCookTimeInput}
+						recipeCookTimeInput={recipeCookTimeInput}
+						handleRecipeCreate={handleRecipeCreate}
+					/>
+				</Modal>
+				:
+				null}
 			<div className={classes.RecipeCard}>
 				{recipes.map(recipe => (
 					<RecipeCard
@@ -30,6 +86,14 @@ const RecipePanel = () => {
 						handleDeleteRecipe={handleDeleteRecipe}
 					/>
 				))}
+				<button
+					onClick={handleShowModal}
+					className={classes.RecipeAddButton}
+				>
+					<svg className={classes.RecipeAddIcon}>
+						<use xlinkHref="/assets/sprite.svg#icon-plus"/>
+					</svg>
+				</button>
 			</div>
 		</React.Fragment>
 	);
